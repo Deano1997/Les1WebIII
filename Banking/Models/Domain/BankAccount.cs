cfdfbf0ba08796c6922977fc3d9f54afb6950f90
Dean Van Damme <dean.vandamme.y9427@student.hogent.be>
2018-09-27 12:15:03 +0200

    Creeër de klasse BankAccount

---
+++ Banking/Models/Domain/BankAccount.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banking.Models.Domain
+{
+    public class BankAccount //visibiliteit is internal als default (zonder public ervoor) => binnen dezelfde assembly
+    {
+        //attributen in java => fields in c#
+        //private string _accountNumber;
+        /*private decimal _balance; // decimal => integer
+
+        public decimal GetBalance()
+        {
+            return _balance;
+        }
+
+        private void SetBalance(decimal value) //private want we mogen niet van buitenaf het bedrag veranderen enkel door storting / afhaling
+        {
+            _balance = value;
+        }*/
+
+        //public decimal Balance { get; private set; } //verkorte notatie van alles hierboven in commentaar, getter => public, setter moeten we specifiek private zetten. visibiliteit geven aan de methode want default is private
+        //{get; private set } is een automatic property.
+        #region Fields
+        public const decimal WithdrawCost = 0.10M;
+        public string AccountNumber { get; set; } //prop + tab + tab
+
+        
+        private decimal _balance;
+        #endregion 
+        #region Properties
+        public decimal Balance //property heeft alleen een get en set dus private decimal _balance moet erbuiten
+        {
+            get { return _balance; }
+            private set
+            { //gebruik maken van een backing field => dit is een full property, men maakt gebruikt van _balance.
+                if (value < 0)
+                    throw new ArgumentException("Invalid value for balance");
+                _balance = value; //binnen een property heeft value speciale betekenis, nl. de parameter die wordt meegegeven.
+            }
+        } 
+        #endregion
+
+        #region Constructors
+        public BankAccount(string accountNumber) //ctor + tab + tab
+        {
+            AccountNumber = accountNumber;
+        }
+
+        public BankAccount(string accountNumber, decimal balance) : this(accountNumber) //andere constructor eerst aanroepen, moet eerste statement zijn gelijk bij java
+        {
+            Balance = balance;
+        }
+        #endregion
+
+
+
+        #region Methods
+        public void Deposit(decimal amount)//,int nrOfTimes = 1) // door een waarde aan toetekennen nrOfTimes wordt dit een optionele parameter, er moet niet noodzakelijk een waarde aan meegegeven worden
+        {
+            Balance += amount;// * nrOfTimes; in commentaar is de optionele parameter.
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            Balance -= amount + WithdrawCost;
+        } 
+        #endregion
+    }
+}
---
+++ Banking/Program.cs
@@ -0,0 +1,32 @@
+﻿using Banking.Models.Domain;
+using System;
+
+namespace Banking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount myBA = new BankAccount("123-123123123-23")
+            {
+                Balance = 50
+            };
+            BankAccount anotherBA = new BankAccount("456-4564564-99",1000);
+
+            Console.WriteLine($"Balance for anotherBA is {anotherBA.Balance} Euro");
+            Console.WriteLine($"The withdrawcost is {BankAccount.WithdrawCost} Euro");
+
+
+            Console.WriteLine($"Balance is currently {myBA.Balance} Euro"); //string interpolation, gebruik maken van $ voor de string, laat het toestaan naar het verwijzen van methode, single inheritance
+            myBA.Deposit(100);
+            //myBA.Deposit(500, 2);
+            Console.WriteLine($"Balance is currently {myBA.Balance} Euro");
+            myBA.Withdraw(50);
+            Console.WriteLine($"Balance is currently {myBA.Balance} Euro");
+            myBA.Withdraw(20);
+            Console.WriteLine($"Balance is currently {myBA.Balance} Euro");
+            Console.ReadKey();
+
+        }
+    }
+}
